d9d7b02b7cb79c26b8cc7a145126f49c056c7ae1
David Lescard <davidlescard@gmail.com>
2014-04-18 11:40:48 -0400

    UI, and some data constructs added

---
+++ app/src/main/java/com/lescard/workoutgenerator/app/components/EquipmentLIST.java
@@ -0,0 +1,62 @@
+package com.lescard.workoutgenerator.app.components;
+
+/**
+ * Created by davidlescard on 4/17/14.
+ */
+public class EquipmentLIST {
+
+    private final int MAX_SIZE = 100;
+    private String equipment[] = new String[MAX_SIZE];
+
+    public void addItem(String s) {
+        int next = findEmpty();
+        equipment[next] = s;
+    }
+
+    public void removeItem(String s) {
+        int locationItem = search(s);
+        if (locationItem == -1) {
+            // do nothing, item doesnt exist
+        } else {
+            equipment[locationItem] = null;
+        }
+    }
+
+    public boolean isFull() {
+        if (findEmpty() == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        boolean result = true;
+        for(int i = 0; i < MAX_SIZE; i++) {
+            if (!equipment[i].equals(null)) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    public int search(String s) {
+        int location = -1;
+        for (int i = 0; i < MAX_SIZE; i++) {
+            if (equipment[i].equals(s)) {
+                location = i;
+            }
+        }
+        return location;
+    }
+
+    private int findEmpty() {
+        int next = -1;
+        for (int i = 0; i < MAX_SIZE-1; i++) {
+            if (equipment[i].equals(null)) {
+               next = i;
+               break;
+            }
+        }
+        return next;
+    }
+}
